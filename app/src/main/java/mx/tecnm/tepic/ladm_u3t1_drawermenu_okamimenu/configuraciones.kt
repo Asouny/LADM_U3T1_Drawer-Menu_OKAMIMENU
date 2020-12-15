9aceb7fbc87f3f174651d275507b02e3145a03a8
Marco Antonio Ramirez Sandoval <60723739+Asouny@users.noreply.github.com>
2020-12-15 10:08:56 -0700

    first
    first

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3t1_drawermenu_okamimenu/configuraciones.kt
@@ -0,0 +1,26 @@
+package mx.tecnm.tepic.ladm_u3t1_drawermenu_okamimenu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class configuraciones : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
